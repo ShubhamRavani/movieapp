9e0c7eeea159bbf18d733857cca41ae0eea7d8b1
Shubham Ravani <shubhamravani67890@gmail.com>
2022-03-04 20:06:29 +0530

    movieapp

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='navbar navbar-dark bg-dark'>
+            <a className='navbar-brand' href="localhost">MovieApp</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
