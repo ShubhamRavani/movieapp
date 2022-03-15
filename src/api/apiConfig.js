344b24f8d380650e2b5a7667c2cfcf65e18f28d5
Shubham Ravani <60728572+ShubhamRavani@users.noreply.github.com>
2022-03-16 01:14:40 +0530

    MovieApp

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'fbdaabaa835507aa7456bf058998996e',
+    apiKey: 'get your api key from themoviedb.org',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
